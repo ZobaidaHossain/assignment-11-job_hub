f426380e844a4a4e561af81373f0ccb0786b7f9e
ZobaidaHossain <zobaida15-13671@diu.edu.bd>
2023-11-09 18:38:54 +0600

    login connect with firebase

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -76,7 +76,7 @@ const navigate=useNavigate();
       <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now</h1>
+          <h1 className="text-5xl font-bold">Login now!</h1>
           
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
