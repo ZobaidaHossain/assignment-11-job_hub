a13415fdf96c23ba4e374b339105682392ac93b6
ZobaidaHossain <zobaida15-13671@diu.edu.bd>
2023-11-09 18:40:38 +0600

    register form addedand firebase setup

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -66,7 +66,7 @@ const [success,setSuccess]=useState('');
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Register now</h1>
+      <h1 className="text-5xl font-bold">Register now!</h1>
       
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
